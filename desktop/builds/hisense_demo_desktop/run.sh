89d1ac4d9fe782c67e6e100373c6b8eabc5f1172
ihorbenko <ihorbenko@plexteq.com>
2020-06-03 15:50:57 +0300

    Removed unneded builds

--- desktop/builds/hisense_demo_desktop/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CURRENT_DIR=$(pwd)
-case ":$LD_LIBRARY_PATH:" in
-  *":$CURRENT_DIR:"*) :;;
-  *) LD_LIBRARY_PATH="$CURRENT_DIR:$LD_LIBRARY_PATH";;
-esac
-export LD_LIBRARY_PATH
-./hisense_demo
