3aece7caa3c424a8f7218530494d102f18e48cdb
vpoddubchak <vpoddubchak@gmail.com>
2020-11-09 17:52:36 +0200

    Set internal type by deafult

--- teevid-client-native/InitialScreen.cpp
+++ teevid-client-native/InitialScreen.cpp
@@ -43,7 +43,7 @@ const QString cAudioSampleFile = "audio-sample-48000.wav";
 InitialScreen::InitialScreen(QWidget *parent) : QWidget(parent), ui(new Ui::InitialScreen)
 {
     // _sourceMode is used inside InitUI, so initialize it's value before the InitUI call
-    _sourceMode = kExternalSourceMode;
+    _sourceMode = kInternalSourceMode;
 
     InitUI();
     //InitSDK();
